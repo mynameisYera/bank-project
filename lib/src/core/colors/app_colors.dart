f1ed2b7afea0fafbb404a802548e63dc57b69823
mynameisYera <yernasip121120@gmail.com>
2024-10-17 01:37:58 +0500

    basic classes created

---
+++ lib/src/core/colors/app_colors.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+class AppColors {
+  //main color black not too
+  static const Color mainColor = Color(0xff121212);
+  //orange button colors
+  static const Color buttonColor = Color(0xffDC712D);
+  //secondary section color
+  static const Color sectionColor = Color(0xff161616);
+}
---
+++ lib/src/core/theme/text_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  //header  text style
+  static TextStyle headerText =
+      TextStyle(fontSize: 20, color: Colors.white, fontWeight: FontWeight.w500);
+  //header  text style
+  static TextStyle simpleText =
+      TextStyle(fontSize: 13, color: Colors.white, fontWeight: FontWeight.w300);
+}
